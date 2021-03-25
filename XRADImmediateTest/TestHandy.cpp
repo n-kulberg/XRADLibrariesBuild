b743146d7be2fadaaf5a3d36436b22af647fb2dc
Nicholas Kulberg <kulberg@yandex.ru>
2021-03-25 20:33:17 +0300

    м

--- XRADImmediateTest/TestHandy.cpp
+++ XRADImmediateTest/TestHandy.cpp
@@ -12,7 +12,6 @@
 */
 #include "pre.h"
 #include <XRADGUI/XRAD.h>
-#include <XRADGUI/Sources/RasterImageFile/RasterImageFile.h>
 #include "TestHandy.h"
 
 //--------------------------------------------------------------
@@ -21,12 +20,18 @@ using namespace xrad;
 
 void	TestRasterImageFile()
 {
+	ComplexFunctionF32	f(8, complexF32(1));
+
+	DisplayMathFunction(f, 0, 1, "F");
+
+/*
 	wstring	fn = GetFileNameRead(L"Open raster image", saved_default_value);
 	file::raster_image	f(fn);
 
 	DisplayMathFunction2D(f.lightness(), "lightness");
 	DisplayMathFunction2D(f.channel(color_channel::hue), "hue");
 	DisplayMathFunction2D(f.rgb(), "image");
+*/
 }
 
 void	TestHandy()
