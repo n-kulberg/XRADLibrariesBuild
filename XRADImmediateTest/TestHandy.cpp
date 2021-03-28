7e6f31b73b8806ccb9f5562440504c912b158a8c
Alexei Kulberg <aleksei.kulberg.work@gmail.com>
2021-03-28 20:29:40 +0300

    Сброс запутанной версии к текущему develop

--- XRADImmediateTest/TestHandy.cpp
+++ XRADImmediateTest/TestHandy.cpp
@@ -17,27 +17,8 @@
 //--------------------------------------------------------------
 
 using namespace xrad;
-
-void	TestRasterImageFile()
+void TestHandy()
 {
-	ComplexFunctionF32	f(8, complexF32(1));
-
-	DisplayMathFunction(f, 0, 1, "F");
-
-/*
-	wstring	fn = GetFileNameRead(L"Open raster image", saved_default_value);
-	file::raster_image	f(fn);
-
-	DisplayMathFunction2D(f.lightness(), "lightness");
-	DisplayMathFunction2D(f.channel(color_channel::hue), "hue");
-	DisplayMathFunction2D(f.rgb(), "image");
-*/
 }
-
-void	TestHandy()
-{
-	TestRasterImageFile();
-}
-
 
 //--------------------------------------------------------------
